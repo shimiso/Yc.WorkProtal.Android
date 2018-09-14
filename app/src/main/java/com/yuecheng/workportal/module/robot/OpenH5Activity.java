03d3e8b4d8fbd269b886f22c70b496b197316f82
shimingsong <simgsg@163.com>
2018-09-14 15:47:15 +0800

    新增h5页面

--- app/src/main/java/com/yuecheng/workportal/module/robot/OpenH5Activity.java
+++ app/src/main/java/com/yuecheng/workportal/module/robot/OpenH5Activity.java
@@ -2,20 +2,19 @@ package com.yuecheng.workportal.module.robot;
 
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.view.View;
-import android.webkit.WebChromeClient;
-import android.webkit.WebViewClient;
 import android.widget.TextView;
 
-import com.yuecheng.workportal.widget.BridgeWebView;
 import com.yuecheng.workportal.R;
 import com.yuecheng.workportal.base.BaseActivity;
-import com.yuecheng.workportal.widget.BridgeWebView;
+import com.yuecheng.workportal.common.JsApi;
+import com.yuecheng.workportal.common.JsEchoApi;
+
+import wendu.dsbridge.DWebView;
 
 
 public class OpenH5Activity extends BaseActivity {
 
-    private BridgeWebView mWebView;
+    private DWebView mWebView;
     private TextView title;
     private String url;
     private String name;
@@ -27,29 +26,19 @@ public class OpenH5Activity extends BaseActivity {
         url = getIntent().getStringExtra("url");
         name = getIntent().getStringExtra("name");
         initView();
-        setWebViewClient();
     }
 
     private void initView() {
         title = findViewById(R.id.title);
         title.setText(name);
-        mWebView = findViewById(R.id.weview);
-        findViewById(R.id.back_iv).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
-        mWebView.addBridgeInterface(new JavaSctiptMethods(OpenH5Activity.this, mWebView));//设置js和android通信桥梁方法
-        //mWebView.loadUrl("file:///android_asset/BridgeWebView/index.html");//本地模板
-        mWebView.loadUrl(url);//动态获取需要打开的链接
+        mWebView = findViewById(R.id.webview);
+        DWebView.setWebContentsDebuggingEnabled(true);
+        mWebView.addJavascriptObject(new JsApi(), "");
+        mWebView.addJavascriptObject(new JsEchoApi(),"echo");
+        mWebView.loadUrl("file:///android_asset/BridgeWebView/js-call-native.html");
+        //mWebView.loadUrl(url);//动态获取需要打开的链接
     }
 
-    private void setWebViewClient() {
-        mWebView.setWebViewClient(new WebViewClient());
-
-        mWebView.setWebChromeClient(new WebChromeClient());
-    }
 
     //使用Webview的时候，返回键没有重写的时候会直接关闭程序，这时候其实我们要其执行的知识回退到上一步的操作
     @Override
