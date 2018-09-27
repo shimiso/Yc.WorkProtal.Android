2b590e07d50e8c41c694e05c420425dd8d632829
huochangsheng <yc#654321>
2018-09-27 14:46:00 +0800

    语音查找人员

--- app/src/main/java/com/yuecheng/workportal/module/robot/view/PeopleDetailTalkListViewHolder.java
+++ app/src/main/java/com/yuecheng/workportal/module/robot/view/PeopleDetailTalkListViewHolder.java
@@ -3,6 +3,7 @@ package com.yuecheng.workportal.module.robot.view;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.yuecheng.workportal.R;
@@ -18,10 +19,12 @@ public class PeopleDetailTalkListViewHolder extends RecyclerView.ViewHolder {
     public TextView phoneTv;
     public LinearLayout send_mail_ll;
     public LinearLayout send_msg_ll;
+    public RelativeLayout details_rl;
 
 
     public PeopleDetailTalkListViewHolder(View itemView) {
         super(itemView);
+        details_rl = (RelativeLayout) itemView.findViewById(R.id.details_rl);
         cti = (CircleTextImage) itemView.findViewById(R.id.cti);
         nameTv = (TextView) itemView.findViewById(R.id.name_tv);
         departmentTv = (TextView) itemView.findViewById(R.id.department_tv);
