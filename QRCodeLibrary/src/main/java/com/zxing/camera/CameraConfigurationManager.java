4f3eb26cd883d7d26fbd67112fc5e58fe6704291
huochangsheng <yc#654321>
2018-10-24 13:50:47 +0800

    解决近距离扫描无法识别

--- QRCodeLibrary/src/main/java/com/zxing/camera/CameraConfigurationManager.java
+++ QRCodeLibrary/src/main/java/com/zxing/camera/CameraConfigurationManager.java
@@ -16,6 +16,7 @@
 
 package com.zxing.camera;
 
+import java.util.List;
 import java.util.regex.Pattern;
 
 import android.content.Context;
@@ -82,13 +83,41 @@ final class CameraConfigurationManager {
 	 * and the planar Y can be used for barcode scanning without a copy in some cases.
 	 */
 	void setDesiredCameraParameters(Camera camera) {
+//		Camera.Parameters parameters = camera.getParameters();
+//		Log.d(TAG, "Setting preview size: " + cameraResolution);
+//		parameters.setPreviewSize(cameraResolution.x, cameraResolution.y);
+//		setFlash(parameters);
+//		setZoom(parameters);
+//		//setSharpness(parameters);
+//		//modify here
+//		camera.setDisplayOrientation(90);
+//		camera.setParameters(parameters);
+
+
+		//解决近距离扫描无法识别
 		Camera.Parameters parameters = camera.getParameters();
+
+		List<Camera.Size> supportedPreviewSizes = parameters.getSupportedPreviewSizes();
+
+		int position =0;
+
+		if(supportedPreviewSizes.size()>2){
+			position=supportedPreviewSizes.size()/2+1;
+		}else {
+			position=supportedPreviewSizes.size()/2;
+		}
+		int width = supportedPreviewSizes.get(position).width;
+		int height = supportedPreviewSizes.get(position).height;
 		Log.d(TAG, "Setting preview size: " + cameraResolution);
-		parameters.setPreviewSize(cameraResolution.x, cameraResolution.y);
+
+		cameraResolution.x=width;
+		cameraResolution.y=height;
+
+		parameters.setPreviewSize(width,height);
+
 		setFlash(parameters);
 		setZoom(parameters);
-		//setSharpness(parameters);
-		//modify here
+
 		camera.setDisplayOrientation(90);
 		camera.setParameters(parameters);
 	}
